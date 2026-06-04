474acfe5ca0e3238feddcb690896d04d7ea60c13
HENRY GABRIEL GONÇALVES <henry_goncalves@estudante.sesisenai.org.br>
2026-06-03 22:13:22 -0300

    finalizaçao da aula guiadae DOM

--- aula guiada DOM/app.js
+++ aula guiada DOM/app.js
@@ -1,54 +1,113 @@
 const btnTema = document.querySelector('#btn-tema');
 
-btnTema.addEventListener('click', () => {  
+btnTema.addEventListener('click', () => {   
 
     document.body.classList.toggle('dark');
 
-
     const modoEscuro = document.body.classList.contains('dark');
 
-    btnTema.textContent = modoEscuro ? ' ☀️ Modo Claro!' : ' 🌙 Modo Escuro!';
+    btnTema.textContent = modoEscuro ? '☀️ Modo Claro' : '🌙 Modo Escuro';
 });
 
-
 const btnToggleInfo = document.querySelector('#btn-toggle-info');
-const infoCorpo = document.querySelector('#info-corpo');
+const infoCorpo    = document.querySelector('#info-corpo');
 
 btnToggleInfo.addEventListener('click', () => {
     const visivel = infoCorpo.style.display !== 'none';
 
-    if(visivel){
+    if (visivel){
         infoCorpo.style.display = 'none';
         btnToggleInfo.textContent = 'Mostrar';
-    }else{
+    } else {
         infoCorpo.style.display = 'block';
         btnToggleInfo.textContent = 'Ocultar';
     }
 
+    // visivel ? 
+    // (infoCorpo.style.display = 'none', btnToggleInfo.textContent = 'Mostrar') 
+    // : (infoCorpo.style.display = 'block', btnToggleInfo.textContent = 'Ocultar');
 });
 
-
-
-
-const btnAumentarJs = document.querySelector('#btn-aumentar-js');
-const barraJs    = document.querySelector('#barra-js');
-const nivelJs    = document.querySelector('b#nivel-js');
+const btnAumentarJs  = document.querySelector('#btn-aumentar-js');
+const barraJs        = document.querySelector('#barra-js');
+const nivelJs       = document.querySelector('#nivel-js');
 
 btnAumentarJs.addEventListener('click', () => {
     const nivelAtual = parseInt(nivelJs.textContent);
 
-
-    if(nivelAtual >= 100){
+    if (nivelAtual >= 100) {
         btnAumentarJs.textContent = 'Javascript Dominado';
-        btnAumentarJs.Disabled = 'true';
+        btnAumentarJs.disabled = 'true';
         return;
-
-        
     }
 
     const novoNivel = Math.min(nivelAtual + 10, 100);
-
+    
     nivelJs.textContent = novoNivel + '%';
 
     barraJs.style.width = novoNivel + '%';
+});
+
+const btnEditarNome       =document.querySelector('#btn-editar-nome');
+const formEdicao       =document.querySelector('#form-edicao');
+const intputNome       =document.querySelector('#intput-nome');
+const intputProfissao       =document.querySelector('#intput-profissao');
+const nomeUsuario       =document.querySelector('#nome-usuario');
+const profissaoUsuario       =document.querySelector('#profissao-usuario');
+
+btnEditarNome.addEventListener('click', () => {
+    formEdicao.style.display = 'block';
+
+    intputNome.value = nomeUsuario.textContent;
+    intputProfissao.value = profissaoUsuario.textContent;
+});
+
+const btnSalvar = document.querySelector('#btn-salvar');
+const btnCancelar = document.querySelector('#btn-cancelar');
+
+btnSalvar.addEventListener('click', () => {
+    const novoNome      =intputNome.value.trim();
+    const novaProfissao      =intputProfissao.value.trim();
+   
+    if(novoNome === ''){
+        alert('Tongão! O nome nao pode estar vazio!');
+        return;
+    }
+    nomeUsuario.textContent = novoNome;
+    profissaoUsuario.textContent = novaProfissao || 'Sem profissao';
+    formEdicao.style.display = 'none';
+});
+
+btnCancelar.addEventListener('click', () => {
+    formEdicao.style.display = 'none';
+});
+
+const btnTrocarFoto = document.querySelector('#btn-trocar-foto');
+const fotoOpcoes = document.querySelector('#foto-opcoes');
+const fotoPerfil = document.querySelector('#foto-perfil');
+
+btnTrocarFoto.addEventListener('click', () => {
+    if(fotoOpcoes.style.display === 'block'){
+        fotoOpcoes.style.display = 'none';
+
+    }else{
+        fotoOpcoes.style.display === 'block';
+        formEdicao.style.display = 'none';
+    }
+});
+
+const todasFotos = document.querySelectorAll('.foto-opcao');
+
+todasFotos.forEach(opcao => {
+    opcao.addEventListener('click', () => {
+
+        const seed = opcao.dataset.seed;
+
+        fotoPerfil.src = `https://api.dicebear.com/7.x/adventurer/svg?seed=${seed}`;
+
+        todasFotos.forEach(f => f.classList.remove('selecionada'));
+        opcao.classList.add('selecionada');
+
+        fotoOpcoes.style.display = 'none';
+    });
 });
