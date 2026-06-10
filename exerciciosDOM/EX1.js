7d22b2d0543f018d9caa679ba43a13ad61a86757
HENRY GABRIEL GONÇALVES <henry_goncalves@estudante.sesisenai.org.br>
2026-06-09 22:05:16 -0300

    add exercicio 1

---
+++ exerciciosDOM/EX1.js
@@ -0,0 +1,8 @@
+const btnTituloJs = document.querySelector('#titulo');
+btnTituloJs.textContent = "Bem-Vindo!";
+
+const btnDescricaoJs = document.querySelector('.descricao');
+btnDescricaoJs.textContent = "Esta é a descrição da página.";
+
+const btnJs = document.querySelector('#btn');
+btnJs.textContent = btn.tagName;
